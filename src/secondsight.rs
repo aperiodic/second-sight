a3a9e6319454d496253dd1ad06af291baa25fff6
aperiodic <dlp@aperiodic.org>
2015-05-08 14:10:31 -0700

    Open X display and query for XRender extension.
    When creating a new XServer wrapper, panic if we can't open the display,
    or if the XRender extension isn't found.
    
    Link against Xrender library in order to use XRenderQueryExtension.
    
    Print a message if we manage to open the display and find the XRender
    extension.

--- src/secondsight.rs
+++ src/secondsight.rs
@@ -1,6 +1,12 @@
 extern crate libc;
 extern crate xlib;
+use xlib::{ XEvent };
+
+use xwrapper::XServer;
+mod xwrapper;
 
 fn main() {
-  println!("gotta learn to make bindings!");
+  let xserv = XServer::new();
+
+  println!("Opened a display and found Xrender extension!");
 }
--- src/xwrapper.rs
+++ src/xwrapper.rs
@@ -1,21 +1,38 @@
+extern crate xlib;
+
 use std::ptr;
-use xlib::{ Display, Window, XOpenDisplay, XDefaultScreenOfDisplay, XRootWindowOfScreen };
+use xlib::{ Display, Window, XDefaultScreenOfDisplay, XOpenDisplay, XQueryExtension, XRootWindowOfScreen };
+
+#[link(name = "Xrender")]
+extern {
+  fn XRenderQueryExtension(display: *mut Display, event: *mut i32, error: *mut i32) -> i32;
+}
 
 pub struct XServer {
-  display: *mut Display;
-  root:         Window;
+  display: *mut Display,
+  root:         Window
 }
 
 impl XServer {
   pub fn new() -> XServer {
     unsafe {
       let display = XOpenDisplay(ptr::null_mut());
+      if display.is_null() {
+        panic!("Could not open display!");
+      }
+
       let screen = XDefaultScreenOfDisplay(display);
       let root = XRootWindowOfScreen(screen);
 
+      let mut render_event = &mut 0;
+      let mut render_error = &mut 0;
+      if XRenderQueryExtension(display, render_event, render_error) == 0 {
+        panic!("No XRender extension!");
+      }
+
       XServer {
-        display: display;
-        root: root;
+        display: display,
+        root: root
       }
     }
   }
